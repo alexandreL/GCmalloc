e8e18714b66652a5760c2c5d0a778d785c44e234
loubey_a <loubey_a@epitech.eu>
2015-06-21 02:10:06 +0200

    auto push

---
+++ GCmalloc/include/gc_malloc.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      /\ ___ /\             */
+/*   gc_malloc.h                                       (  o   o  )            */
+/*                                                      \  >#<  /             */
+/*   By: alex <alexandre.loubeyres@gmail.com>           /       \             */
+/*                                                     /         \       ^    */
+/*   Created: 2015/06/18 15:30:16 by alex             |           |     //    */
+/*   Updated: 2015/06/18 15:53:47 by alex              \         /    //      */
+/*                                                      ///  ///   --         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GC_MALLOC_H_
+# define GC_MALLOC_H_
+
+typedef struct	s_gc_m
+{
+  void		*data;
+  int		id;
+  int		size;
+  struct s_gc_m	*prev;
+  struct s_gc_m	*next;  
+}		t_gc_m
+
+#endif /* GC_MALLOC_H_ */
